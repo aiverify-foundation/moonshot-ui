09f417af69f6b579919a0398305b8bc9a61f4925
aminmdlahir <aminurrashid_mohamed_lahir@imda.gov.sg>
2024-01-11 12:31:37 +0800

    split drop down menu

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -40,47 +40,97 @@ function Menu() {
       {isMenuOpen ? (
         <div
           style={{
-            position: 'absolute',
             display: 'flex',
-            flexDirection: 'column',
             backgroundColor: 'rgba(0, 0, 0, 0.85)',
-            width: 160,
+            position: 'absolute',
             top: '100%',
             left: 20,
             boxShadow: '0px 3px 6px #00000029',
           }}>
-          <div
-            style={{
-              padding: '12px 25px',
-              paddingTop: 13,
-              borderBottom: '1px solid rgba(255, 255, 255, 0.2)',
-            }}>
-            Create Cookbook
+          <div style={{ borderRight: '1px solid rgba(255, 255, 255, 0.06)', padding: 20 }}>
+            <h3
+              style={{
+                fontWeight: 800,
+                fontSize: 16,
+                paddingLeft: 20,
+                marginBottom: 20,
+                color: '#068afd',
+              }}>
+              Benchmarking
+            </h3>
+            <div
+              style={{
+                display: 'flex',
+                backgroundColor: 'rgba(0, 0, 0, 0.85)',
+                flexDirection: 'column',
+                width: 200,
+                top: '100%',
+                boxShadow: '0px 3px 6px #00000029',
+              }}>
+              <div
+                style={{
+                  padding: '12px 25px',
+                  paddingTop: 13,
+                  borderBottom: '1px solid rgba(255, 255, 255, 0.2)',
+                }}>
+                Create Cookbook
+              </div>
+              <div
+                style={{
+                  padding: '12px 25px',
+                  borderBottom: '1px solid rgba(255, 255, 255, 0.2)',
+                }}>
+                Create Recipe
+              </div>
+              <div
+                style={{
+                  padding: '12px 25px',
+                  borderBottom: '1px solid rgba(255, 255, 255, 0.2)',
+                }}>
+                Create Endpoint
+              </div>
+              <div
+                style={{
+                  padding: '12px 25px',
+                  paddingBottom: 12,
+                }}>
+                Run Cookbook
+              </div>
+            </div>
           </div>
-          <div
-            style={{
-              padding: '12px 25px',
-              borderBottom: '1px solid rgba(255, 255, 255, 0.2)',
-            }}>
-            Create Recipe
-          </div>
-          <div
-            style={{
-              padding: '12px 25px',
-              borderBottom: '1px solid rgba(255, 255, 255, 0.2)',
-            }}>
-            Create Endpoint
-          </div>
-          <div
-            style={{
-              padding: '12px 25px',
-              paddingBottom: 12,
-              borderBottom: '1px solid rgba(255, 255, 255, 0.2)',
-            }}>
-            Run Cookbook
-          </div>
-          <div style={{ padding: '12px 25px', paddingBottom: 12 }}>
-            New Chat Session
+          <div style={{ padding: 20 }}>
+            <h3
+              style={{
+                fontWeight: 800,
+                fontSize: 16,
+                paddingLeft: 20,
+                marginBottom: 20,
+                color: '#f94d4d',
+              }}>
+              Red Teaming
+            </h3>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                width: 200,
+                top: '100%',
+              }}>
+              <div
+                style={{
+                  padding: '12px 25px',
+                  paddingTop: 13,
+                  borderBottom: '1px solid rgba(255, 255, 255, 0.2)',
+                }}>
+                Manual Red Teaming
+              </div>
+              <div
+                style={{
+                  padding: '12px 25px',
+                }}>
+                Automated Red Teaming
+              </div>
+            </div>
           </div>
         </div>
       ) : null}
