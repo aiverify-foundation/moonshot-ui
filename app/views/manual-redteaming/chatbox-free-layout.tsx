543278b1c64d0bedb2206fd44c3c77cbe84dab41
Amin <aminurrashid_mohamed_lahir@imda.gov.sg>
2024-02-12 08:33:56 +0800

    centralize free layout chats if less that 4 chats

--- app/views/manual-redteaming/chatbox-free-layout.tsx
+++ app/views/manual-redteaming/chatbox-free-layout.tsx
@@ -38,25 +38,41 @@ function ChatboxFreeLayout(props: ChatFreeLayoutProps) {
   const dispatch = useAppDispatch();
   const windowsMap = useAppSelector((state) => state.windows.map);
 
-
   useEffect(() => {
     const numberOfChats = chatSession.chats.length;
     if (numberOfChats) {
       const viewportWidth = window.innerWidth; // Get the viewport width
       const margin = 20; // Left and right margin
-      const adjustedViewportWidth = viewportWidth - (margin * 2); // Adjust viewport width for margins
+      const adjustedViewportWidth = viewportWidth - margin * 2; // Adjust viewport width for margins
       const chatBoxWidth = 400; // Fixed width for each ChatBox
       const chatBoxHeight = 450; // Fixed height for each ChatBox
-      const spacing = (adjustedViewportWidth - chatBoxWidth) / Math.max(numberOfChats - 1, 1); // Calculate spacing, avoid division by zero
+      const spacing =
+        (adjustedViewportWidth - chatBoxWidth) / Math.max(numberOfChats - 1, 1); // Calculate spacing, avoid division by zero
+
+      //These 3 are for layout calculation where number of chats is 4 or less
+      //Centralize them side by side
+
+      const gap = 10; // Gap between ChatBoxes
+      const totalWidthNeeded =
+        chatBoxWidth * numberOfChats + gap * (numberOfChats - 1); // Total width needed for all ChatBoxes and gaps
+      const startX = (adjustedViewportWidth - totalWidthNeeded) / 2 + margin; // Calculate starting X position for centralization
 
       const chatWindows: Record<string, WindowData> = {};
       chatSession.chats.forEach((id, index) => {
         if (windowsMap[getWindowId(id)]) return; // if window has size and position in application state from previous launch, skip setting defaults
         let xyPos: [number, number] = [0, 0];
-        const xpos = (index * spacing) + margin; // Calculate x position for even spread
+        const xpos =
+          numberOfChats > 4
+            ? index * spacing + margin
+            : startX + index * (chatBoxWidth + gap); // Calculate x position for even spread
         const ypos = index % 2 === 0 ? 150 : 200; // Alternate y position
         xyPos = [xpos, ypos];
-        chatWindows[getWindowId(id)] = [...xyPos, chatBoxWidth, chatBoxHeight, 0];
+        chatWindows[getWindowId(id)] = [
+          ...xyPos,
+          chatBoxWidth,
+          chatBoxHeight,
+          0,
+        ];
       });
       dispatch(updateWindows(chatWindows));
     }
