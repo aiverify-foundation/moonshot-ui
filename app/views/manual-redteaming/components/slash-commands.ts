c8a44f61c761adfb6c4c08af84c4d524db612a6a
Amin <aminurrashid_mohamed_lahir@imda.gov.sg>
2024-02-13 16:23:05 +0800

    update command descriptions

--- app/views/manual-redteaming/components/slash-commands.ts
+++ app/views/manual-redteaming/components/slash-commands.ts
@@ -8,18 +8,18 @@ export enum SlashCommand {
   MINIMIZE_PROMPT = 'mininput',
   CLOSE_SESSION = 'exit',
   DARK_MODE_ON = 'darkmode-on',
-  DARK_MODE_OFF = 'darkmodeo-ff',
+  DARK_MODE_OFF = 'darkmode-off',
 }
 
 export const descriptionByCommand: Record<SlashCommand, string> = {
-  [SlashCommand.PROMPT_TEMPLATE]: 'Change the prompt template',
-  [SlashCommand.CLEAR_PROMPT_TEMPLATE]: 'Clear the prompt template',
-  [SlashCommand.CHAT_LAYOUT_MODE_FREE]: 'Change the chat layout mode to free',
-  [SlashCommand.CHAT_LAYOUT_MODE_SLIDE]: 'Change the chat layout mode to slide',
-  [SlashCommand.RESET_LAYOUT_MODE]: 'Reset the chat layout mode',
-  [SlashCommand.MAXIMIZE_PROMPT]: 'Maximize the prompt',
-  [SlashCommand.MINIMIZE_PROMPT]: 'Minimize the prompt',
-  [SlashCommand.CLOSE_SESSION]: 'Close the session',
+  [SlashCommand.PROMPT_TEMPLATE]: 'Update prompt template',
+  [SlashCommand.CLEAR_PROMPT_TEMPLATE]: 'Clear prompt template',
+  [SlashCommand.CHAT_LAYOUT_MODE_FREE]: 'Change chat layout to Free mode',
+  [SlashCommand.CHAT_LAYOUT_MODE_SLIDE]: 'Change chat layout to Slide mode',
+  [SlashCommand.RESET_LAYOUT_MODE]: 'Reset chat positions',
+  [SlashCommand.MAXIMIZE_PROMPT]: 'Maximize prompt input',
+  [SlashCommand.MINIMIZE_PROMPT]: 'Minimize prompt input',
+  [SlashCommand.CLOSE_SESSION]: 'Close session',
   [SlashCommand.DARK_MODE_ON]: 'Turn on Dark Mode',
   [SlashCommand.DARK_MODE_OFF]: 'Turn off Dark Mode',
 };
