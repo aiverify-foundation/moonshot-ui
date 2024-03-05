f8fb8f50dc13b0d738958bf3b4e26f3e571cc0dc
aminmdlahir <aminurrashid_mohamed_lahir@imda.gov.sg>
2024-03-05 16:48:30 +0800

    add comments to zindex system in constants file

--- app/views/moonshot-desktop/constants.ts
+++ app/views/moonshot-desktop/constants.ts
@@ -13,12 +13,14 @@ export enum WindowIds {
   CREATE_SESSION = 'createSession',
 }
 
+// Reserved z-indexes
+// Try to follow these zindex layers as much as possible
 export const Z_Index: ZIndex = {
-  Base: 1,
-  Level_1: 100,
-  Level_2: 200,
-  FocusedWindow: 998,
-  Top: 999,
+  Base: 1, // first layer - for any elements that need to sit between the desktop background and icons (background logo image, etc)
+  Level_1: 100, // second layer - specifically the desktop icons layer
+  Level_2: 200, // third layer - all windows should be on this layer
+  FocusedWindow: 998, // fourth layer - any focused window, should be set to this layer
+  Top: 999, // fifth layer - for any elements that needs to be at the top most layer (modal popup, tooltip, etc)
 };
 
 export const defaultWindowWidthHeight: {
