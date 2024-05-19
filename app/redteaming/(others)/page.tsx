16d72fd57ef4dd3a2fc8aca3f975a6768b8fe53e
aminmdlahir <aminurrashid_mohamed_lahir@imda.gov.sg>
2024-05-20 03:30:39 +0800

    add back to home link to rt menu

--- app/redteaming/(others)/page.tsx
+++ app/redteaming/(others)/page.tsx
@@ -2,49 +2,57 @@ import Link from 'next/link';
 import { IconName } from '@/app/components/IconSVG';
 import { colors } from '@/app/views/shared-components/customColors';
 import { SubmenuButton } from '@/app/views/shared-components/submenuButton/submenuButton';
+import BackToHomeButton from '@/app/views/shared-components/backToHomeButton/backToHomeButton';
 
 export default function RedteamingHomePage() {
   return (
-    <section className="flex flex-col items-center gap-2.5">
-      <h3 className="text-moonpurplelight tracking-widest text-[1.4rem]">
-        benchmark with moonshot
-      </h3>
-      <Link
-        href="/redteaming/sessions/new"
-        style={{ width: '40%' }}>
-        <SubmenuButton
-          width="100%"
-          text="Start New Session"
-          menuIconName={IconName.Spacesuit}
-          btnColor={colors.moongray[950]}
-          hoverBtnColor={colors.moongray[800]}
-          textColor={colors.white}
-        />
-      </Link>
-      <Link
-        href="/redteaming/sessions"
-        style={{ width: '40%' }}>
-        <SubmenuButton
-          width="100%"
-          text="View Past Sessions"
-          menuIconName={IconName.HistoryClock}
-          btnColor={colors.moongray[950]}
-          hoverBtnColor={colors.moongray[800]}
-          textColor={colors.white}
-        />
-      </Link>
-      <Link
-        href="/redteaming/attackmodules"
-        style={{ width: '40%' }}>
-        <SubmenuButton
-          width="100%"
-          text="View Attack Modules"
-          menuIconName={IconName.MoonAttackStrategy}
-          btnColor={colors.moongray[950]}
-          hoverBtnColor={colors.moongray[800]}
-          textColor={colors.white}
-        />
-      </Link>
-    </section>
+    <>
+      <header>
+        <Link href="/">
+          <BackToHomeButton colors={colors} />
+        </Link>
+      </header>
+      <section className="flex flex-col items-center gap-2.5">
+        <h3 className="text-moonpurplelight tracking-widest text-[1.4rem]">
+          benchmark with moonshot
+        </h3>
+        <Link
+          href="/redteaming/sessions/new"
+          style={{ width: '40%' }}>
+          <SubmenuButton
+            width="100%"
+            text="Start New Session"
+            menuIconName={IconName.Spacesuit}
+            btnColor={colors.moongray[950]}
+            hoverBtnColor={colors.moongray[800]}
+            textColor={colors.white}
+          />
+        </Link>
+        <Link
+          href="/redteaming/sessions"
+          style={{ width: '40%' }}>
+          <SubmenuButton
+            width="100%"
+            text="View Past Sessions"
+            menuIconName={IconName.HistoryClock}
+            btnColor={colors.moongray[950]}
+            hoverBtnColor={colors.moongray[800]}
+            textColor={colors.white}
+          />
+        </Link>
+        <Link
+          href="/redteaming/attackmodules"
+          style={{ width: '40%' }}>
+          <SubmenuButton
+            width="100%"
+            text="View Attack Modules"
+            menuIconName={IconName.MoonAttackStrategy}
+            btnColor={colors.moongray[950]}
+            hoverBtnColor={colors.moongray[800]}
+            textColor={colors.white}
+          />
+        </Link>
+      </section>
+    </>
   );
 }
