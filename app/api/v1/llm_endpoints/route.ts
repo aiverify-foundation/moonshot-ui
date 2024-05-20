3baf769f5698fae761b2b6b279d48ef398712511
Amin <aminurrashid_mohamed_lahir@imda.gov.sg>
2024-05-20 13:07:52 +0800

    endpoint id url param

--- app/api/v1/llm_endpoints/route.ts
+++ app/api/v1/llm_endpoints/route.ts
@@ -1,9 +1,12 @@
+import { NextRequest } from 'next/server';
 import config from '@/moonshot.config';
 export const dynamic = 'force-dynamic';
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const id = searchParams.get('id');
   const response = await fetch(
-    `${config.webAPI.hostURL}${config.webAPI.basePathLLMEndpoints}`,
+    `${config.webAPI.hostURL}${config.webAPI.basePathLLMEndpoints}${id ? `/${id}` : ''}`,
     {
       method: 'GET',
     }
