915da0214795a1f7a5569dc2795233d5712e9e93
Amin <aminurrashid_mohamed_lahir@imda.gov.sg>
2024-05-24 15:07:18 +0800

    update page level error ui

--- app/benchmarking/error.tsx
+++ app/benchmarking/error.tsx
@@ -1,6 +1,10 @@
 'use client'; // Error components must be Client Components
 
+import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
+import { Icon, IconName } from '@/app/components/IconSVG';
+import { colors } from '@/app/views/shared-components/customColors';
+import { Modal } from '@/app/views/shared-components/modal/modal';
 
 export default function Error({
   error,
@@ -9,21 +13,29 @@ export default function Error({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
+  const router = useRouter();
   useEffect(() => {
     // Log the error to an error reporting service
     console.error(error);
   }, [error]);
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }>
-        Try again
-      </button>
-    </div>
+    <Modal
+      heading="Something went wrong!"
+      bgColor={colors.moongray['800']}
+      textColor="#FFFFFF"
+      primaryBtnLabel="Try again"
+      enableScreenOverlay
+      onCloseIconClick={() => router.push('/')}
+      onPrimaryBtnClick={() => reset()}>
+      <div className="flex items-start gap-2 pt-4">
+        <Icon
+          name={IconName.Alert}
+          size={30}
+          color="red"
+        />
+        <p className="text-[0.9rem]">{error.message}</p>
+      </div>
+    </Modal>
   );
 }
