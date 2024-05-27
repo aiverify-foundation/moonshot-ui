79a0fcd620e442e792d251ddb2d77676d047a833
aminmdlahir <aminurrashid_mohamed_lahir@imda.gov.sg>
2024-05-27 13:27:09 +0800

    disable cache for fetch cookbooks

--- app/benchmarking/cookbooks/page.tsx
+++ app/benchmarking/cookbooks/page.tsx
@@ -5,7 +5,8 @@ export const dynamic = 'force-dynamic';
 
 async function fetchCookbooks() {
   const response = await fetch(
-    `${config.webAPI.hostURL}${config.webAPI.basePathCookbooks}?count=true`
+    `${config.webAPI.hostURL}${config.webAPI.basePathCookbooks}?count=true`,
+    { cache: 'no-store' }
   );
   const result = await processResponse<Cookbook[]>(response);
   return result;
