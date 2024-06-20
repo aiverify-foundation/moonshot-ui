6aee45f08f71bf3306944fb3a2347c28cf528a81
aminmdlahir <aminurrashid_mohamed_lahir@imda.gov.sg>
2024-06-20 13:47:44 +0800

    prompt count screen component initial test spec

---
+++ app/views/benchmarking/__tests__/benchmarkMainCookbooksPromptCount.test.tsx
@@ -0,0 +1,55 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import {
+  CookbooksProvider,
+  useCookbooks,
+} from '@/app/views/benchmarking/contexts/cookbooksContext';
+import { useAppSelector } from '@/lib/redux';
+import { BenchmarkMainCookbooksPromptCount } from '@/app/views/benchmarking/benchmarkMainCookbooksPromptCount';
+
+jest.mock('@/lib/redux', mockRedux);
+
+function mockRedux() {
+  return {
+    useAppSelector: jest.fn(),
+  };
+}
+
+const mockChangeView = jest.fn();
+
+const mockCookbooks = [
+  {
+    id: 'cb-id-1',
+    name: 'Mock Cookbook One',
+    description: 'Mock description',
+    recipes: ['rc-id-1'],
+    total_prompt_in_cookbook: 10,
+  },
+  {
+    id: 'cb-id-2',
+    name: 'Mock Cookbook Two',
+    description: 'Mock description',
+    recipes: ['rc-id-2'],
+    total_prompt_in_cookbook: 20,
+  },
+];
+
+describe('BenchmarkMainCookbooksPromptCount', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  it('shows loading animation', () => {
+    const mockOneAlreadySelectedCookbooksFromState = mockCookbooks;
+    (useAppSelector as jest.Mock).mockImplementation(
+      () => mockOneAlreadySelectedCookbooksFromState
+    );
+    render(
+      <CookbooksProvider>
+        <BenchmarkMainCookbooksPromptCount changeView={mockChangeView} />
+      </CookbooksProvider>
+    );
+    expect(screen.getByText(/loading/i)).toBeInTheDocument();
+
+    screen.debug();
+  });
+});
