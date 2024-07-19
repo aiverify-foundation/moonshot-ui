e73c470eb5cceffd59e473c85e4ff8d9e49847f0
aminmdlahir <aminurrashid_mohamed_lahir@imda.gov.sg>
2024-07-19 10:09:18 +0800

    fix lint in error-utils test

--- app/lib/_tests_/error-utils.test.ts
+++ app/lib/_tests_/error-utils.test.ts
@@ -1,65 +1,65 @@
-import { isErrorWithMessage, toErrorWithMessage } from "../error-utils";
+import { isErrorWithMessage, toErrorWithMessage } from '@/app/lib/error-utils';
 
-test("should return true, given Error instance using new keyword", () => {
-  const error = new Error("mock error");
+test('should return true, given Error instance using new keyword', () => {
+  const error = new Error('mock error');
   const result = isErrorWithMessage(error);
   expect(result).toBe(true);
 });
 
-test("should return true, given Error instance", () => {
-  const error = Error("mock error");
+test('should return true, given Error instance', () => {
+  const error = Error('mock error');
   const result = isErrorWithMessage(error);
   expect(result).toBe(true);
 });
 
-test("should return true, given object with message property", () => {
+test('should return true, given object with message property', () => {
   const error = { message: 'mock error', cause: 'mock cause' };
   const result = isErrorWithMessage(error);
   expect(result).toBe(true);
 });
 
-test("should return false, given object without message property", () => {
+test('should return false, given object without message property', () => {
   const error = { error: 'mock error' };
   const result = isErrorWithMessage(error);
   expect(result).toBe(false);
 });
 
-test("should return false, given string", () => {
+test('should return false, given string', () => {
   const error = 'mock error';
   const result = isErrorWithMessage(error);
   expect(result).toBe(false);
 });
 
-test("should return error with message, given Error instance", () => {
-  const errorInstance = new Error("mock error", { cause: 'mock cause' });
+test('should return error with message, given Error instance', () => {
+  const errorInstance = new Error('mock error', { cause: 'mock cause' });
   const result = toErrorWithMessage(errorInstance);
   expect(result).toBe(errorInstance);
   expect(result.message).toBe('mock error');
 });
 
-test("should return error with message, given non Error instance - string", () => {
+test('should return error with message, given non Error instance - string', () => {
   const errorString = 'mock error';
   const result = toErrorWithMessage(errorString);
   expect(result.message).toBe(errorString);
 });
 
-test("should return error with message, given non Error instance - object with message property", () => {
+test('should return error with message, given non Error instance - object with message property', () => {
   const errorObject = { message: 'mock error', cause: 'mock cause' };
   const result = toErrorWithMessage(errorObject);
   expect(result).toBe(errorObject);
   expect(result.message).toBe('mock error');
 });
 
-test("should return error with message, given non Error instance - object without message property", () => {
+test('should return error with message, given non Error instance - object without message property', () => {
   const errorObject = { error: 'mock cause' };
   const result = toErrorWithMessage(errorObject);
   expect(result).not.toBe(errorObject);
   expect(result.message).toEqual('{"error":"mock cause"}');
 });
 
-test("should return error with message, given non Error instance - object without message property, but cannot be JSON stringified", () => {
-  const errorObject: {error: string, test?: object} = { error: 'mock cause' };
-  errorObject.test = errorObject
+test('should return error with message, given non Error instance - object without message property, but cannot be JSON stringified', () => {
+  const errorObject: { error: string; test?: object } = { error: 'mock cause' };
+  errorObject.test = errorObject;
   const result = toErrorWithMessage(errorObject);
   expect(result).not.toBe(errorObject);
   expect(result.message).not.toBeUndefined();
