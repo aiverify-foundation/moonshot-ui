b55f0c8ea01d2fed6258bfe4b36304faeb82b2a4
aminmdlahir <aminurrashid_mohamed_lahir@imda.gov.sg>
2024-08-28 07:54:35 +0800

    required endpoints tooltip

--- app/benchmarking/components/cookbookSelectionItem.tsx
+++ app/benchmarking/components/cookbookSelectionItem.tsx
@@ -1,6 +1,13 @@
 import { useState } from 'react';
 import { Icon, IconName } from '@/app/components/IconSVG';
+import { Tooltip, TooltipPosition } from '@/app/components/tooltip';
 import { colors } from '@/app/customColors';
+
+const endpoints = [
+  'Together Llama Guard 7B Assistant',
+  'Together Llama3 8B Chat HF',
+  'LLM Judge - OpenAI GPT4',
+];
 
 type CookbookSelectionItemProps = {
   cookbook: Cookbook;
@@ -12,7 +19,7 @@ type CookbookSelectionItemProps = {
 function CookbookSelectionItem(props: CookbookSelectionItemProps) {
   const { cookbook, selected, onSelect, onAboutClick } = props;
   const [isSelected, setIsSelected] = useState(selected);
-
+  const requiredEndpoints = cookbook.required_endpoints_in_cookbook;
   function handleClick(
     e: React.MouseEvent | React.ChangeEvent<HTMLInputElement>
   ) {
@@ -39,7 +46,35 @@ function CookbookSelectionItem(props: CookbookSelectionItemProps) {
       <header className="flex flex-basis-[100%] justify-between">
         <div className="flex gap-2 text-white">
           <Icon name={IconName.Book} />
-          <h3 className="font-bold">{cookbook.name}</h3>
+          <h3 className="font-bold ">
+            {cookbook.name.length > 40
+              ? `${cookbook.name.substring(0, 40)}...`
+              : cookbook.name}
+          </h3>
+          {cookbook.id == 'mlc-ai-safety' && (
+            <Tooltip
+              position={TooltipPosition.right}
+              offsetLeft={10}
+              content={
+                <div className="p-4">
+                  <h3 className="text-black font-bold mb-2">Requires</h3>
+                  <ul className="text-gray-700">
+                    {endpoints.map((endpoint) => (
+                      <li key={endpoint}>{endpoint}</li>
+                    ))}
+                  </ul>
+                </div>
+              }>
+              <Icon
+                size={25}
+                name={IconName.SolidBox}
+                color={colors.moonpurplelight}
+                style={{
+                  animation: 'pulse 1.5s ease-in-out infinite',
+                }}
+              />
+            </Tooltip>
+          )}
         </div>
         <input
           readOnly
