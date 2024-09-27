e91d6485cbfd2a3696ae6928fc53c329d8198311
Amin <aminurrashid_mohamed_lahir@imda.gov.sg>
2024-09-27 08:11:33 +0000

    update next.config and env.local

--- next.config.js
+++ next.config.js
@@ -1,21 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || undefined,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || undefined,
-  webpack: (config, { dev, isServer }) => {
-    if (dev && !isServer) {
-      const originalEntry = config.entry
-      config.entry = async () => {
-        const entries = await originalEntry()
-        if (entries['main.js'] && !entries['main.js'].includes('./client/dev/webpack-hot-middleware-client')) {
-          entries['main.js'].unshift('./client/dev/webpack-hot-middleware-client')
-        }
-        return entries
-      }
-    }
-    return config
-  },
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || ''
 }
 
 module.exports = nextConfig
