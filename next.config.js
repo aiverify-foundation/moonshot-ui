b3dc59a68fafe4ae2a48ebb0b1a26e6b6c16a330
aminmdlahir <aminurrashid_mohamed_lahir@imda.gov.sg>
2024-10-03 09:37:23 +0800

    rewrite in next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || ''
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  async rewrites() {
+    if (process.env.NEXT_PUBLIC_BASE_PATH) {
+      return [
+        {
+          source: `${process.env.NEXT_PUBLIC_BASE_PATH}/api/:path*`,
+          destination: `/api/:path*`,
+        },
+      ];
+    } else {
+      // Return an empty array if rewrites are not enabled
+      return [];
+    }
+  },
 }
 
 module.exports = nextConfig
