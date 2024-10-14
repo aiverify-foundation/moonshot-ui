027f8917bffb0bc9a552636c7009334c7c9b2161
aminmdlahir <aminurrashid_mohamed_lahir@imda.gov.sg>
2024-10-14 18:24:29 +0800

    update endpoint type, model is optional

--- app/global.d.ts
+++ app/global.d.ts
@@ -82,7 +82,7 @@ type LLMEndpoint = {
   max_calls_per_second: number;
   max_concurrency: number;
   created_date: string;
-  model: string;
+  model?: string;
   params?: Record<string, string | number | boolean>;
 };
 
@@ -92,7 +92,7 @@ type LLMEndpointFormValues = {
   name: string;
   uri: string;
   token: string | undefined;
-  model: string;
+  model?: string;
   max_calls_per_second: string;
   max_concurrency: string;
   params?: string;
