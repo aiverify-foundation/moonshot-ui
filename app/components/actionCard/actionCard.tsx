76e1e862afd37f986058392c8b3302b80ef640e0
aminmdlahir <aminurrashid_mohamed_lahir@imda.gov.sg>
2024-11-20 10:57:43 +0800

    responsive entry banners

--- app/components/actionCard/actionCard.tsx
+++ app/components/actionCard/actionCard.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Icon, IconName } from '@/app/components/IconSVG';
 import styles from './styles/actionCard.module.css';
+import { cn } from '@/app/lib/cn';
+import { useIsResponsiveBreakpoint } from '@/app/hooks/useIsResponsiveBreakpoint';
 
 type ActionCardProps = {
   title: string;
@@ -15,16 +17,18 @@ type ActionCardProps = {
   iconSize?: number;
   height?: number;
   style?: React.CSSProperties;
+  className?: string;
   onClick?: () => void;
 };
 
 function ActionCard(props: ActionCardProps) {
+  const screenSize = useIsResponsiveBreakpoint();
   const {
     title,
     titleSize,
     description,
     iconName,
-    iconSize = 50,
+    iconSize = screenSize === 'sm' || screenSize === 'md' ? 40 : 50,
     iconColor = '#FFFFFF',
     cardColor = '#000000',
     textColor = '#FFFFFF',
@@ -33,10 +37,16 @@ function ActionCard(props: ActionCardProps) {
     actionText,
     style,
     onClick,
+    className,
   } = props;
+
   return (
     <figure
-      className={styles.card}
+      className={cn(
+        styles.card,
+        `${screenSize === 'sm' || screenSize === 'md' ? '!h-[260px]' : ''}`,
+        className
+      )}
       style={{ backgroundColor: cardColor, height, ...style }}
       onClick={onClick}>
       <Icon
