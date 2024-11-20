2044a80c9506e20354e1121c6cf0e2ea87d8d5df
aminmdlahir <aminurrashid_mohamed_lahir@imda.gov.sg>
2024-11-20 17:08:48 +0800

    type check window in hook

--- app/hooks/useOrientation.ts
+++ app/hooks/useOrientation.ts
@@ -1,13 +1,19 @@
 import { useEffect, useState } from 'react';
 
+function initializeOrientation() {
+  if (typeof window === 'undefined') return 'landscape';
+  return window.screen.orientation.type.includes('portrait')
+    ? 'portrait'
+    : 'landscape';
+}
+
 export function useOrientation() {
   const [orientation, setOrientation] = useState<'portrait' | 'landscape'>(
-    window?.screen?.orientation?.type?.includes('portrait')
-      ? 'portrait'
-      : 'landscape'
+    initializeOrientation()
   );
   useEffect(() => {
     const handleOrientationChange = () => {
+      if (typeof window === 'undefined') return 'landscape';
       setOrientation(
         window.screen.orientation.type.includes('portrait')
           ? 'portrait'
