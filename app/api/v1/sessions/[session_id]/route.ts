878bc728e1c26a005b04e490851ef373208a6ba8
Penn Han Lee <lee_penn_han@imda.gov.sg>
2025-06-05 22:41:45 +0800

    regex validation

--- app/api/v1/sessions/[session_id]/route.ts
+++ app/api/v1/sessions/[session_id]/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest } from 'next/server';
+import { isValidId } from '@/app/api/v1/apiUtils';
 import config from '@/moonshot.config';
 export const dynamic = 'force-dynamic';
 
@@ -6,6 +7,9 @@ export async function GET(request: NextRequest) {
   let session_id: string;
   try {
     session_id = request.nextUrl.pathname.split('/')[4];
+    if (!isValidId(session_id)) {
+      throw new Error("Invalid session id")
+    }
   } catch (error) {
     return new Response('Unable to get session id from url path', {
       status: 500,
